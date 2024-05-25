4850b49ccaa017c562ca0ff6a692e2a777e0ab9f
bilyana-ancheva <bilyana.ancheva@bootcamp-upgrade.com>
2024-05-25 14:58:08 +0200

    button Add New Meal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,10 +6,14 @@ import AuthRoute from "./AuthRoute/AuthRoute";
 import Profile from "./Profile";
 import axios from "axios";
 
+
 function App() {
   const [user, setUser] = useState(null);
   const [listUsers, setListUsers] = useState([]);
   const [meal, setMeal] = useState([]);
+  const [nombre, setNombre] = useState("");
+  const [categoria, setCategoria] = useState("");
+  const [descripcion, setDescripcion] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -30,6 +34,32 @@ function App() {
     fetchData();
   }, []);
 
+   useEffect(() => {
+  const postData = async () => {
+    const newMeal = {
+      id: "{newMeal.length}", // Ensure this ID is unique if you need to specify it
+      nombre: "",
+      categoria: "",
+      descripcion: ""
+    };
+
+    try {
+      const response = await axios.post('https://664c9e9635bbda10988127e7.mockapi.io/comida', newMeal);
+      console.log('POST response:', response.data);
+      
+      // Optionally update the state with the new meal
+      setMeal(prevMeal => [...prevMeal, response.data]);
+    } catch (error) {
+      console.error('Error al realizar la solicitud POST:', error);
+    }
+  };
+
+  postData();
+}, []); // Empty dependency array to run this effect only once on mount
+
+  const handleClick=() => {
+    postData(); 
+  }
   return (
     <div>
       <NavBar/>
@@ -47,6 +77,25 @@ function App() {
       <div>
        <div>
       <h1>Lista de Comidas</h1>
+      <input
+          type="text"
+          placeholder="Nombre"
+          value={nombre}
+          onChange={(e) => setNombre(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Categoria"
+          value={categoria}
+          onChange={(e) => setCategoria(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Descripcion"
+          value={descripcion}
+          onChange={(e) => setDescripcion(e.target.value)}
+        />
+      <button onClick={handleClick}>Add New Meal</button>
       <ul>
         {meal.map(meal => (
           <li key={meal.id}>
@@ -56,7 +105,10 @@ function App() {
           </li>
         ))}
       </ul>
+      
+
     </div>
+    
     </div>
     </div>
   );
