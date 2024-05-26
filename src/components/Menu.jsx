e38bc237ef796131b8f864c465629030a6d110d7
bilyana-ancheva <bilyana.ancheva@bootcamp-upgrade.com>
2024-05-26 14:47:36 +0200

    final commit 26-05-2024

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,10 +2,11 @@ const Menu = ({ items }) => {
     return (
       <div>
         {items.map((menuItem) => {
-          const { id, nombre, descripcion } = menuItem;
+          const { id, nombre, categoria, descripcion } = menuItem;
           return (
             <article key={id}>
               <div>
+                <p>{categoria}</p>
                 <header>
                   <h4>{nombre}</h4>
                 </header>
